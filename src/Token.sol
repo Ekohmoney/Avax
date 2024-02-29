8c314e10ea625ee5a63bf3d1fba220a19e879028
Ekohmoney <53713848+Ekohmoney@users.noreply.github.com>
2024-02-29 20:12:42 +0100

    Update Token.sol

--- src/Token.sol
+++ src/Token.sol
@@ -1,32 +1,36 @@
-// SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    address owner;
+  address owner;
 
-    constructor() ERC20("EkohToken", "EKO") {
-        owner = msg.sender;
-    }
+  constructor() ERC20("EkohToken", "EKO") {
+    owner = msg.sender;
+  }
 
-    modifier onlyOwner() {
-        require(owner == msg.sender);
-        _;
-    }
+  modifier onlyOwner() {
+    require(owner == msg.sender);
+    _;
+  }
 
-    function mint(address _to, uint _amount) external onlyOwner {
-        _mint(_to, _amount);
-    }
+  function mint(address _to, uint _amount) external onlyOwner {
+    _mint(_to, _amount);
+  }
 
-    function burn(uint _amount) public {
-        if (balanceOf(msg.sender) < _amount){
-            revert ("Insufficient funds in balance");
-        }
-        _burn(msg.sender, _amount);
+  function burn(uint _amount) public {
+    if (balanceOf(msg.sender) < _amount) {
+      revert("Insufficient funds in balance");
     }
+    _burn(msg.sender, _amount);
+  }
 
-    function decimals() public view virtual override  returns(uint8){
-        return 0;
-    }
+  function transfer(address recipient, uint amount) public {
+    _transfer(msg.sender, recipient, amount); // Call internal transfer function
+  }
+}
+
+  function decimals() public view virtual override returns (uint8) {
+    return 0;
+  }
 }
